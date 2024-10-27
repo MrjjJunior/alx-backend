93855b4f0e051eb76c55eddf61bab9a54b19d6ab
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-27 23:40:53 +0200

    task 0 helper

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Python Script on Pagination """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """ Helper function """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
