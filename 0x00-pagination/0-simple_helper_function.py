cb939800475ba25fd77065367be9350ce1fbcc86
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-27 23:44:00 +0200

    Python annotation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ Helper function """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
